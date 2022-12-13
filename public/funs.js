e40f76d787daab26fbd39e6dfcbbd7b63cda7d5c
Glitch (graceful-blossom-animal) <none>
2022-12-13 05:45:05 +0000

    🦭📴 Updated with Glitch: day 13, part 1

--- public/funs.js
+++ public/funs.js
@@ -1183,7 +1183,59 @@
       }
     },
     day13: {
-      part1: () => {},
+      part1: (data) => {
+        const packets = data.trim().split('\n\n').map(group => group.split('\n').map(pack => JSON.parse(pack)));
+        console.log(packets);
+        const compare = (left, right) => {
+          for (let i = 0; i < left.length; i++) {
+            const l = left[i];
+            const r = right[i];
+            if (typeof r === 'undefined') {
+              return -1;
+            }
+            const isLNum = typeof l === 'number';
+            const isRNum = typeof r === 'number';
+            if (isLNum && isRNum) {
+              if (l < r) {
+                return 1;
+              } else if (l > r) {
+                return -1;
+              }
+            } else {
+              let al = l;
+              if (isLNum) {
+                al = [l];
+              }
+              let ar = r;
+              if (isRNum) {
+                ar = [r];
+              }
+              const nest = compare(al, ar);
+              if (nest !== 0) {
+                return nest;
+              } 
+            }
+          }
+          if (left.length < right.length) {
+            return 1;
+          }
+          return 0;
+        };
+        const good = [];
+        const bad = [];
+        packets.forEach((pair, i) => {
+          const left = pair[0];
+          const right = pair[1];
+          const order = compare(left, right);
+          if (order > 0) {
+            good.push(i + 1);
+          } else if (order < 0) {
+            bad.push(i + 1);
+          }
+        });
+        console.log(good, bad);
+        return good.reduce((sum, index) => sum + index, 0);
+      },
       part2: () => {}
     },
     day14: {
