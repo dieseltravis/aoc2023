c7e2281b097bce450afca06f446ab9e796446871
Glitch (graceful-blossom-animal) <none>
2022-12-21 06:50:26 +0000

    👂🥺 Updated with Glitch: day 21, part is started

--- public/funs.js
+++ public/funs.js
@@ -1595,7 +1595,7 @@
     },
     day21: {
       part1: (data) => {
-        const rx = /(\d+)|(\w+) ([+\-\/\*]) (\w+)/;
+        const rx = /(\d+)|(\w+) ([+\-/*]) (\w+)/;
         const op = {
           '+': (m1, m2) => m1 + m2,
           '-': (m1, m2) => m1 - m2,
@@ -1609,7 +1609,7 @@
           };
           const match = pair[1].match(rx);
           if (typeof match[1] === 'undefined') {
-            monkey.m1 = match[2],
+            monkey.m1 = match[2];
             monkey.op = op[match[3]];
             monkey.m2 = match[4];
           } else {
@@ -1641,11 +1641,96 @@
         if (safety <= 0) {
           console.warn('safety hit!');
         }
-        
+
         console.log(numonks, fumonks);
         return (monkeys.find(m => m.name === 'root').num);
       },
-      part2: () => {}
+      part2: (data) => {
+        const rx = /(\d+)|(\w+) ([+\-/*]) (\w+)/;
+        const op = {
+          '+': (m1, m2) => m1 + m2,
+          '-': (m1, m2) => m1 - m2,
+          '*': (m1, m2) => m1 * m2,
+          '/': (m1, m2) => m1 / m2
+        };
+        const monkeys = data.trim().split('\n').map(line => line.split(':').map(side => side.trim())).map(pair => {
+          const monkey = {
+            name: pair[0],
+            isDone: false
+          };
+          const match = pair[1].match(rx);
+          if (typeof match[1] === 'undefined') {
+            monkey.m1 = match[2];
+            monkey.op = op[match[3]];
+            monkey.m2 = match[4];
+          } else if (monkey.name !== 'humn') {
+            monkey.num = +match[1];
+          }
+          if (monkey.name === 'root') {
+            monkey.op = (m1, m2) => m1 === m2;
+          }
+          return monkey;
+        });
+        const numonks = monkeys.filter(m => typeof m.num !== 'undefined');
+        const fumonks = monkeys.filter(m => typeof m.num === 'undefined');
+        let safety1 = 1000;
+        let lastlen = -1;
+        let numlen = numonks.length;
+        while (lastlen !== numlen && safety1--) {
+          numonks.filter(n => !n.isDone).forEach(n => {
+            fumonks.filter(f => f.m1 === n.name).forEach(f => {
+              f.n1 = n.num;
+            });
+            fumonks.filter(f => f.m2 === n.name).forEach(f => {
+              f.n2 = n.num;
+            });
+            n.isDone = true;
+          });
+          fumonks.filter(f => typeof f.n1 !== 'undefined' && typeof f.n2 !== 'undefined' && !f.isDone).forEach(f => {
+            f.num = f.op(f.n1, f.n2);
+            numonks.push(f);
+          });
+          lastlen = numlen;
+          numlen = numonks.length;
+        }
+        if (safety1 <= 0) {
+          console.warn('safety1 hit!');
+        }
+        console.log(numonks, fumonks);
+
+        let safety2 = 1000;
+        const monlen = monkeys.length;
+        const funk = (h, m) => {
+          const monkeys = m.slice();
+          const numonks = monkeys.filter(m => typeof m.num !== 'undefined');
+          const fumonks = monkeys.filter(m => typeof m.num === 'undefined');
+
+          while (monlen > numlen && safety2--) {
+            numonks.filter(n => !n.isDone).forEach(n => {
+              fumonks.filter(f => f.m1 === n.name).forEach(f => {
+                f.n1 = n.num;
+              });
+              fumonks.filter(f => f.m2 === n.name).forEach(f => {
+                f.n2 = n.num;
+              });
+              n.isDone = true;
+            });
+            fumonks.filter(f => typeof f.n1 !== 'undefined' && typeof f.n2 !== 'undefined' && !f.isDone).forEach(f => {
+              f.num = f.op(f.n1, f.n2);
+              numonks.push(f);
+            });
+            numlen = numonks.length;
+          }
+          if (safety2 <= 0) {
+            // true
+            console.warn('safety2 hit!');
+          }
+          return monkeys;
+        }
+        console.log(funk(1, monkeys));
+
+        return (monkeys.find(m => m.name === 'root').n1);
+      }
     },
     day22: {
       part1: () => {},
